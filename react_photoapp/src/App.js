a2bb7fdf5a4659db81171940cf1585f3b309caba
Pulkit Narula <43206958+pulkitnarula13@users.noreply.github.com>
2022-12-29 14:48:26 -0800

    Completed

--- react_photoapp/src/App.js
+++ react_photoapp/src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import DogImage from './components/DogImage';
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    async function fetchImages() {
+      const response = await fetch('https://reqres.in/api/users');
+      const data = await response.json();
+      console.log(data)
+      setImages(data.data);
+    }
+    fetchImages();
+  }, []);
+
   return (
     <div>
-      <DogImage />
+      {images.map((image) => (
+        <DogImage
+          key={image.id}
+          image={image.avatar}
+          likes={0}
+          onLike={() => console.log('Like clicked')}
+        />
+      ))}
     </div>
   );
 }
 
 export default App;
-
-
--- react_photoapp/src/components/DogImage.js
+++ react_photoapp/src/components/DogImage.js
@@ -1,21 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
-function DogImage() {
-    const [imageUrl, setImageUrl] = useState('');
+function DogImage(props) {
     const [likes, setLikes] = useState(0);
 
-    useEffect(() => {
-        async function fetchImage() {
-        const response = await fetch('https://dog.ceo/api/breeds/image/random');
-        const data = await response.json();
-        setImageUrl(data.message);
-        }
-        fetchImage();
-    }, []);
 
     return (
         <div>
-        <img src={imageUrl} alt="Random dog" />
+        <img src={props.image} alt="Random dog" />
         <button onClick={() => setLikes(likes + 1)}>
             Like ({likes})
         </button>
